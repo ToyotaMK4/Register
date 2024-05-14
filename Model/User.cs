c04d16bd2c1c762e8d042cab65749242a30e20f3
AfruzCoder <husanovafruz1@gmail.com>
2024-05-14 09:17:33 +0500

    rff

---
+++ Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Register.Models
+{
+    internal class User
+    {
+        public string Name { get; set}
+        public string Gmail { get; set; }
+    }
+    public var Pasword { get; set}
+}
